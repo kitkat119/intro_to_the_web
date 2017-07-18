171f542f927807276aac255ba7fddfbde136ec29
kitkat119 <gprydal@yahoo.co.uk>
2017-07-18 12:52:41 +0100

    posted params

--- app.rb
+++ app.rb
@@ -10,11 +10,15 @@ end
 
 get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
-  erb(:index)
+  erb :index
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @name = params[:name]
-  erb(:index)
+  erb :index
+end
+
+get '/cat-form' do
+  erb :cat_form
 end
